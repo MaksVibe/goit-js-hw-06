c04c51edca313dcd28f4197e28f759e6c83ed53e
MaksVibe <parunovmaksym@gmail.com>
2021-11-10 17:52:32 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+  email: document.querySelector("[name=email]"),
+  password: document.querySelector("[name=password]"),
+  submitBtn: document.querySelector("[type=submit]"),
+};
 
+refs.form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const user = {
+    email: refs.email.value,
+    password: refs.password.value,
+  };
+  if (!refs.email.value || !refs.password.value) {
+    refs.submitBtn.disabled;
+    alert("All fields must be filled in!");
+  }
+  refs.form.reset();
+  console.log(user);
+});
