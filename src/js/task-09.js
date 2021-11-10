54234b99bff525643ff58857ead7c54b2d608269
MaksVibe <parunovmaksym@gmail.com>
2021-11-10 18:17:27 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColor = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const body = document.querySelector("body");
+
+changeColor.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+});
