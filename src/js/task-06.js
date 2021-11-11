9bfa83b28b27b7baf5cfc6b6f0838972af72ef0a
MaksVibe <parunovmaksym@gmail.com>
2021-11-11 23:34:43 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,16 +1,10 @@
 const input = document.querySelector("#validation-input");
-const maxLength = input.getAttribute("data-length");
+const maxLength = Number(input.getAttribute("data-length"));
 
 input.addEventListener("blur", () => {
-  if (maxLength >= input.value.length > 0) {
+  input.classList.add("invalid");
+  if (maxLength === input.value.length) {
     input.classList.remove("invalid");
     input.classList.add("valid");
-  }
-  if (input.value.length > maxLength) {
-    input.classList.add("invalid");
-  }
-  if (input.value.length === 0) {
-    input.classList.remove("invalid");
-    input.classList.remove("valid");
   }
 });
