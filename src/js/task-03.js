d8eef787a0bd73c91184bf6b0e0f4ccdc05a49f7
MaksVibe <parunovmaksym@gmail.com>
2021-11-11 22:08:29 +0200

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -17,18 +17,16 @@ const images = [
 ];
 
 const gallery = document.querySelector(".gallery");
-const liMarkup = images
-  .map(({ url, alt }) => {
-    const liRef = document.createElement("li");
-    gallery.append(liRef);
-    const imgRef = document.createElement("img");
-    imgRef.src = url;
-    imgRef.alt = alt;
-    imgRef.classList.add("img");
-    liRef.append(imgRef);
-  })
-  .join("");
-gallery.insertAdjacentHTML("afterbegin", liMarkup);
+images.map((image) => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `
+  <li>
+    <img src="${image.url}" alt="${image.alt}" width="280"/>
+  </li>
+`
+  );
+});
 gallery.style.display = "inline-flex";
-gallery.style.width = "90%";
+gallery.style.maxWidth = "90%";
 gallery.style.gap = "20px";
